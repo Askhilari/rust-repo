6185cca0f79293c39182e194955f97bd10721603
RN Khilari <askhilari433@gmail.com>
2025-04-27 23:56:43 -0500

    Hello World using cargo.

---
+++ hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\aclap\Documents\rust-Codes\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\aclap\Documents\rust-Codes\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+C:\Users\aclap\Documents\rust-Codes\hello-cargo\target\debug\hello-cargo.exe: C:\Users\aclap\Documents\rust-Codes\hello-cargo\src\main.rs
